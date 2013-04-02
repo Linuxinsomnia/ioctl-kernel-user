00e857cabdc6f776b7c662358cd4fffd5d33ddf1
Sandipan Patra <introducingsandipan@gmail.com>
2013-04-02 23:36:38 +0530

    Update ioctl.h

--- ioctl.h
+++ ioctl.h
@@ -8,8 +8,8 @@
 
 /* IOCTL defines */
 #define MY_MAGIC_NO   'P'
-#define IOCTL_WRITE_REG   _IOW(MAJOR_NUM, 1, int *)
-#define IOCTL_WRITE_REG   _IOR(MAJOR_NUM, 1, int *)
+#define IOCTL_WRITE_REG   _IOW(MY_MAGIC_NO, 1, int *)
+#define IOCTL_WRITE_REG   _IOR(MY_MAGIC_NO, 1, int *)
 
 
 #endif //__IOCTL_H
